5266996e85039b1cf30f968884c4cd47fb99634d
Oleg Bedrin <simplavero@gmail.com>
2021-12-15 22:52:24 +0300

    got rid of bundles requests

--- main.py
+++ main.py
@@ -4,15 +4,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from opensea import OpenseaAPI
+from opensea import utils as opensea_utils
 
 # create an object to interact with the Opensea API (need an api key)
 api = OpenseaAPI(apikey=os.environ["OPENSEA_API_KEY"])
 
-# fetch multiple events
-from opensea import utils as opensea_utils
-
 period_start = opensea_utils.datetime_utc(2021, 11, 6, 14, 25)
 period_end = opensea_utils.datetime_utc(2021, 11, 6, 14, 30)
+
 result = api.events(
     collection_slug="cryptopunks",
     occurred_after=period_start,
@@ -22,5 +21,4 @@ result = api.events(
     export_file_name="sales.json",
 )
 
-# fetch multiple bundles
-result = api.bundles(limit=2)
+print(result)
