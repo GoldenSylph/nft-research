c970902d9353df61a8f2652d237f2fb22334351b
Oleg Bedrin <simplavero@gmail.com>
2021-12-15 22:49:44 +0300

    adding fetching of sales

--- main.py
+++ main.py
@@ -8,19 +8,18 @@ from opensea import OpenseaAPI
 # create an object to interact with the Opensea API (need an api key)
 api = OpenseaAPI(apikey=os.environ["OPENSEA_API_KEY"])
 
-# fetch collection stats
-result = api.collection_stats(collection_slug="cryptopunks")
-
 # fetch multiple events
 from opensea import utils as opensea_utils
 
 period_start = opensea_utils.datetime_utc(2021, 11, 6, 14, 25)
 period_end = opensea_utils.datetime_utc(2021, 11, 6, 14, 30)
 result = api.events(
+    collection_slug="cryptopunks",
     occurred_after=period_start,
     occurred_before=period_end,
     limit=10,
-    export_file_name="events.json",
+    event_type="successful",
+    export_file_name="sales.json",
 )
 
 # fetch multiple bundles
